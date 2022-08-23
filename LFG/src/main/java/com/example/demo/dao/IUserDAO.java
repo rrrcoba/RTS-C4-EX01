91526c8dd55fe656f1ee651311d90cbc3b95b210
Roger <108723939+rrrcoba@users.noreply.github.com>
2022-08-23 12:49:48 +0200

    Delete LFG directory

--- LFG/src/main/java/com/example/demo/dao/IUserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.dto.User;
-
-public interface IUserDAO extends JpaRepository<User, Long>{
-
-}
